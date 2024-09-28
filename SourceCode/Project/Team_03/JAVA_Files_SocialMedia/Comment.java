39d373fee55f617fd4784a41becc10f607f1c6c8
Manaswi10-reddy <167505869+Manaswi10-reddy@users.noreply.github.com>
2024-09-28 10:40:43 +0530

    changes

--- SourceCode/Project/Team_03/JAVA_Files_SocialMedia/Comment.java
+++ SourceCode/Project/Team_03/JAVA_Files_SocialMedia/Comment.java
@@ -1,5 +1,7 @@
 package SocialMedia;
 
+import java.sql.ResultSet;
+
 public class Comment {
 	private Int commentId;
 	private Int postId;
@@ -45,3 +47,47 @@ public void display() {
 	System.out.println("Content: "+content);
 }
 }
+public class CommentDAO{
+	public List<Comment>getAllComment(){
+		List<Comment> c=new ArrayList();
+		try {
+			connection con=DbConnection.getConnection();
+			Statement stmt=con.createStatement();
+			ResultSet rs=stmt.executeQuery("select * from Comment");
+			while(rs.next())
+			{
+				int commentId=rs.getInt("commentId");
+				int postId=rs.getInt("postId");
+				int userId=rs.getInt("userId);
+				String content=rs.getString("content");
+				Comment co=new Comment(commentId,postID,userId,content);
+				c.add(co);
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return c;
+	}
+}
+public static void main(String[] args) {
+	List<comment> al=new ArrayList<>();
+	System.out.format("%-5s %-15s %-10s %s\n","commentID","postId","userID","content");
+	CommentDAO dao=new CommentDAO();
+	al=dao.getAllUser();
+	for(Comment it:al) {
+		System.out.format("%-5s %-15s %-10s %s\n",it.getCommentId(),it.getPostId(),it.getUserId(),it.getContent());
+	}
+}
+public class DdConnection{
+	public static Connection getConnection()throws ClassNotFoundException,SQLException{
+		ResourceBundle rb = ResourceBundle.getBundle("mysql");
+		String url = rb.getString("db.url");
+		String username = rb.getString("db.username");
+		String password = rb.getString("db.password");
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriveManager.getConnection(url,username,password);
+		return con;
+	}
+}
+
